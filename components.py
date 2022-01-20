6119725dbe3af2abc5b2cd30a3e762af7b04b800
Aspect13 <ad13box@gmail.com>
2022-01-20 13:27:49 +0300

    Fixing scheduling plugin names

--- components.py
+++ components.py
@@ -3,6 +3,6 @@ from flask import render_template
 
 def render_security_test_create(context, slot, payload):
     return render_template(
-        'security_scheduling:security_test_create.html',
+        'scheduling:security_test_create.html',
         config=payload
     )
