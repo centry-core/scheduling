2eb0e799636b327323f51965f346ab1b35ca9106
Aspect13 <ad13box@gmail.com>
2023-04-06 18:24:00 +0400

    db shared metadata fix

--- init_db.py
+++ init_db.py
@@ -3,5 +3,5 @@ from tools import db
 
 def init_db():
     from .models.schedule import Schedule
-    db.Base.metadata.create_all(bind=db.engine)
+    db.get_shared_metadata().create_all(bind=db.engine)
 
